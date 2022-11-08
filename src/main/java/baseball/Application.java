26a944e3452f1c4a107592fcfaac762b9943a736
서정희 <sjhlko@naver.com>
2022-11-09 03:26:16 +0900

    feat: 핵심기능 1번 완성 3개의 서로 다른 임의의 숫자를 생성하여 3자리수를 만들어 정답으로 설정한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,21 @@
 package baseball;
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
+    private List<Integer> answer = new ArrayList<>();
     public void printStartMessage(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public void makeAnswer(){
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
     }
 
 
