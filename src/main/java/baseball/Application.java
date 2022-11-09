80cc8b30523d394a4010edf7cd0ce3a48e06d2f1
서정희 <sjhlko@naver.com>
2022-11-09 15:13:51 +0900

    feat: 출력 2 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,14 +6,20 @@ import java.util.List;
 
 public class Application {
     private List<Integer> answer = new ArrayList<>();
+    private String GuessedNumber = "";
     public void printStartMessage(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+    public void getNumber(){
+        System.out.println("숫자를 입력해주세요 : ");
+        this.GuessedNumber = Console.readLine();
+    }
+
     public void makeAnswer(){
-        while (answer.size() < 3) {
+        while (this.answer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!answer.contains(randomNumber)) {
-                answer.add(randomNumber);
+            if (!this.answer.contains(randomNumber)) {
+                this.answer.add(randomNumber);
             }
         }
     }
@@ -21,5 +27,10 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application application = new Application();
+        application.makeAnswer();
+        System.out.println(application.answer);
+        application.makeAnswer();
+        System.out.println(application.answer);
     }
 }
