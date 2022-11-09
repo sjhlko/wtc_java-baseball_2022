e386a8c4e2fdff59679337d2990794fd9939dc77
서정희 <sjhlko@naver.com>
2022-11-09 15:14:08 +0900

    feat: 출력 4 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,7 +14,10 @@ public class Application {
         System.out.println("숫자를 입력해주세요 : ");
         this.GuessedNumber = Console.readLine();
     }
-
+    public void printSuccessMessage(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
     public void makeAnswer(){
         while (this.answer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
